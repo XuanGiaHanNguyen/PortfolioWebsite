111366243b5ef037b79287ceeeb6b826cb8bd041
XuanGiaHanNguyen <han316@usf.edu>
2025-01-18 21:42:31 -0500

    fixing

---
+++ frontend/src/page/Landing.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import '../index.css'
+
+function Landing () {
+    return(
+        <div className='' 
+        style={{backgroundColor: 'FFF7EB'}}>
+            <p>Hello</p>
+        </div>
+    )
+}
+
+export default Landing
